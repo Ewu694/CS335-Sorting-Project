56df707a6d48022e8de2fb736b58ba988f53e519
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 01:39:22 -0500

    Update MergeSort.hpp
    Fixed MergeSort

--- MergeSort.hpp
+++ MergeSort.hpp
@@ -1,45 +1,29 @@
-#ifndef MERGESORT_HPP
-#define MERGESORT_HPP
-
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <algorithm>
 
-class MergeSort
-{
-    public:
-        int mergeSort(std::vector<int>& nums, int& duration)
-        {
-            auto start = std::chrono::steady_clock::now();
+void mergeSortHelper(std::vector<int>&nums){
+    if (nums.size() <= 1)
+        return;
+    
+    std::vector<int>::size_type center = nums.size()/2;
+    std::vector<int> left(nums.begin(), nums.begin() + center);
+    std::vector<int> right(nums.begin()+center, nums.end());
+    mergeSortHelper(left);
+    mergeSortHelper(right);
+    std::merge(left.begin(), left.end(), right.begin(), right.end(), nums.begin());
+}
 
-            bool isEven;
-            if(nums.size() % 2 == 0)
-                isEven = true;
+int mergeSort ( std::vector<int>& nums, int& duration){
+    auto t1 = std::chrono::high_resolution_clock::now();
+    
+    int half = nums.size()/2;
+    mergeSortHelper(nums);
+    
+    auto t2 = std::chrono::high_resolution_clock::now(); // Update the stop time
+    auto dur = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    duration = dur.count();
 
-            std::vector<int> temp(nums.size());
-            mergeSortHelper(nums, temp, 0, nums.size() - 1);
-
-            auto end = std::chrono::steady_clock::now();
-            auto diff = end - start;
-
-            duration = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
-
-            if(isEven)
-                return nums[(nums.size() / 2) - 1];
-            return nums[nums.size() / 2];
-        }
-
-        void mergeSortHelper(std::vector<int>& nums, std::vector<int>& temp, int start, int end) //used as a helper to implement mergeSort recursively
-        {
-            if (start < end) 
-            {    
-                int mid = start + (end - start) / 2;
-
-                mergeSortHelper(nums, temp, start, mid);
-                mergeSortHelper(nums, temp, mid + 1, end);
-
-                std::merge(nums, temp, start, mid, end);
-            }
-        }
-};
-#endif
+    return nums[nums.size() % 2 == 0 ? nums.size() / 2 - 1 : nums.size() / 2]; //Returns the lesser of the two medians
+}
