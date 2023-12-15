bd71d0869dfc536957e25876bafba2ee1c5dcd06
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 02:12:48 -0500

    Update HalfHeapSort.hpp
    PLEASE WORK

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -3,52 +3,50 @@
 #include <iostream>
 #include <chrono>
 
-void percDown(std::vector<int> &heap, std::vector<int>::size_type hole)
-{
-   int temp = heap[0];/temp hold on first element 
+int leftChild(int i) 
+   return 2 * i; 
 
-   // Iterate through the heap starting from the root
-   for (int child; hole * 2 < heap.size(); hole = child) // Iterate through the heap starting from the root
-   {
-      child = hole * 2;
-      if (child + 1 < heap.size() && heap[child + 1] < heap[child])
-         child++;
-      if (temp > heap[child])
-         heap[hole] = heap[child];
-      else
-         break;
-   }
-   heap[hole] = temp; // Place temp back in the right place
+void percDown(std::vector<int>& a, int i) 
+{
+    int child;
+    int tmp;
+    for(tmp = std::move( a[ i ] ); leftChild( i ) < a.size(); i = child) 
+    {
+        child = leftChild( i );
+        if( child != a.size() - 1 && a[child] > a[child + 1])
+            child++;
+        if(tmp > a[ child ])
+            a[ i ] = std::move(a[ child ]);
+        else
+            break;
+    }
+    a[i] = std::move(tmp);
 }
 
-void buildHeap(std::vector<int> &heap)
-{
-   for (int i = (heap.size() - 1) / 2; i > 0; --i)// Iterate through the non-leaf elements of the heap
-   {
-      heap[0] = heap[i];
-      percDown(heap, i);
-   }
+
+void buildHeap (std::vector<int>& heap) {
+    for (int i = heap.size() / 2; i > 0; --i) 
+        percDown(heap, i);
 }
 
-// Implementation of HalfHeapSort
-int halfHeapSort(std::vector<int> &nums, int &duration)
-{
-   auto start_time = std::chrono::high_resolution_clock::now();
 
-   nums.push_back(nums[0]);
-   buildHeap(nums);
-   int middle = (nums.size() - 2) / 2;
-   for (int j = 0; j < middle; ++j)
-   {
-      nums[0] = nums[nums.size() - 1];
-      nums.pop_back();
-      percDown(nums, 1);
-   }
+int halfHeapSort (std::vector<int>& nums, int& duration) {
+    auto start = std::chrono::steady_clock::now();
 
-   nums.erase(nums.begin());
-    
-   auto end_time = std::chrono::high_resolution_clock::now();
-   duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+    nums.push_back(nums[0]);
+    buildHeap(nums);
+    int halfsize = nums.size() / 2;
+   
+    for (int i = nums.size() - 1; i > halfsize + 1; --i) 
+    {
+        std::swap(nums[1], nums[i]);
+        nums.pop_back();
+        percDown(nums, 1);
+    }
 
-   return nums[0];
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
+
+    return nums[1];
 }
