930b57b9b3679f2f691578f927eef16d4d66a69e
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 01:52:07 -0500

    Update HalfHeapSort.hpp
    HalfHeapSorted...

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -1,58 +1,54 @@
-
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <chrono>
 
-    int halfHeapSort(std::vector<int>& nums, int& duration)
-    {
-        auto start = std::chrono::steady_clock::now();
+void percDown(std::vector<int> &heap, std::vector<int>::size_type hole)
+{
+   int temp = heap[0];/temp hold on first element 
 
-        std::vector<int>::size_type newSize = nums.size() / 2;
-        std::swap(nums[0], nums[nums.size() - 1]); // Move the first element to the end
-        nums.resize(newSize); // Reduce the size to half
+   // Iterate through the heap starting from the root
+   for (int child; hole * 2 < heap.size(); hole = child) // Iterate through the heap starting from the root
+   {
+      child = hole * 2;
+      if (child + 1 < heap.size() && heap[child + 1] < heap[child])
+         child++;
+      if (temp > heap[child])
+         heap[hole] = heap[child];
+      else
+         break;
+   }
+   heap[hole] = temp; // Place temp back in the right place
+}
 
-        buildHeap(nums);
+void buildHeap(std::vector<int> &heap)
+{
+   for (int i = (heap.size() - 1) / 2; i > 0; --i)// Iterate through the non-leaf elements of the heap
+   {
+      heap[0] = heap[i];
+      percDown(heap, i);
+   }
+}
 
-        // Perform half heap sort by removing elements
-        for (int i = 0; i < newSize / 2; ++i) {
-        std::swap(nums[0], nums[nums.size() - 1]); // Swap root with the last element
-        nums.pop_back(); // Delete the last element (smallest)
-        percDown(nums, 0); // Adjust the heap
-    }
-        duration = newSize / 2; // Update the duration
+// Implementation of HalfHeapSort
+int halfHeapSort(std::vector<int> &nums, int &duration)
+{
+   auto start_time = std::chrono::high_resolution_clock::now();
 
-        auto end = std::chrono::steady_clock::now();
-        auto diff = end - start;
+   nums.push_back(nums[0]);
+   buildHeap(nums);
+   int middle = (nums.size() - 2) / 2;
+   for (int j = 0; j < middle; ++j)
+   {
+      nums[0] = nums[nums.size() - 1];
+      nums.pop_back();
+      percDown(nums, 1);
+   }
 
-        duration = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
-        return nums[0]; // Return the median at the root
-    }
-    void percDown(std::vector<int>& heap, std::vector<int>::size_type hole)
-    {
-        std::vector<int>::size_type child;
-        int tmp = std::move(heap[hole]); // Store the value at hole
+   nums.erase(nums.begin());
+    
+   auto end_time = std::chrono::high_resolution_clock::now();
+   duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
 
-        while (2 * hole + 1 < heap.size()) 
-        {
-            child = 2 * hole + 1;
-            if (child != heap.size() - 1 && heap[child + 1] < heap[child])
-                ++child; // Move to the right child if it exists and is smaller
-            if (heap[child] < tmp) 
-                heap[hole] = std::move(heap[child]); // Move child up
-            else 
-                break;
-        hole = child;
-        }
-        heap[hole] = std::move(tmp); // Place the value at the final hole
-    }
-    // parameter "hole" is the index of the hole.
-    // percDown precondition: value to be inserted into hole is stored in heap at index 0. The hole itself may be in an unspecified state - i.e. it doesn't matter what's in it since you'll be overwriting it anyway.
-    // percDown postcondition: hole has been moved into correct place and value has been inserted into hole.
-    void buildHeap(std::vector<int>& heap)
-    {
-        for(int i = heap.size() / 2 - 1; i >= 0; --i)
-        {
-            percDown(heap, i);
-        }
-    }
+   return nums[0];
+}
