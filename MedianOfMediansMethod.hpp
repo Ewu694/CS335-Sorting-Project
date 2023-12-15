8c8318d5125e438ce9d0ee7803789720caa710bb
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 02:04:23 -0500

    Update MedianOfMediansMethod.hpp

--- MedianOfMediansMethod.hpp
+++ MedianOfMediansMethod.hpp
@@ -1,76 +1,68 @@
-#ifndef MEDIANOFMEDIANSMETHOD_HPP
-#define MEDIANOFMEDIANSMETHOD_HPP
-
 #include <chrono>
 #include "QuickSelect.hpp"
 
-class MedianOfMediansMethod
+int medianOfMediansMethod(std::vector<int>& nums, int& duration)
 {
-    public:
-        int medianOfMediansMethod(std::vector<int>& nums, int& duration)
-        {
-            auto start = std::chrono::steady_clock::now();
+    auto start = std::chrono::steady_clock::now();
 
-            int median = medianOfMedians(nums, nums.begin(), nums.end());
+    int median = medianOfMedians(nums, nums.begin(), nums.end());
 
-            auto end = std::chrono::steady_clock::now();
-            auto diff = end - start;
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
 
-            duration = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
             
-            return median;
-        }
-        int medianOfMedians(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high)
-        {
-            bool isEven;
-            if(nums.size() % 2 == 0)
-                isEven = true;
+    return median;
+}
 
-            int numElements = std::distance(low, high);
+int medianOfMedians(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high)
+{
+    bool isEven;
+    if(nums.size() % 2 == 0)
+        isEven = true;
 
-            if (numElements <= 5) 
-            {
-                std::sort(low, high);
-                return *(low + numElements / 2);
-            }
+    int numElements = std::distance(low, high);
+    if (numElements <= 5) 
+    {
+        std::sort(low, high);
+        return *(low + numElements / 2);
+    }
 
-            int numGroups = (numElements + 4) / 5;
-            std::vector<int> medians(numGroups);
+    int numGroups = (numElements + 4) / 5;
+    std::vector<int> medians(numGroups);
 
-            for (int i = 0; i < numGroups; ++i) 
-            {
-                auto groupStart = low + i * 5;
-                auto groupEnd = std::min(groupStart + 5, high);
-                medians[i] = quickSelect(nums, groupStart, groupEnd, (groupEnd - groupStart) / 2);
-            }
+    for (int i = 0; i < numGroups; ++i) 
+    {
+        auto groupStart = low + i * 5;
+        auto groupEnd = std::min(groupStart + 5, high);
+        medians[i] = quickSelect(nums, groupStart, groupEnd, (groupEnd - groupStart) / 2);
+    }
 
-            int result = quickSelect(medians, medians.begin(), medians.end(), medians.size() / 2);
-            return result;
-        }
-        int medianOfFive(int a, int b, int c, int d, int e)
-        {
-            if (a > b) 
-                std::swap(a, b);
-            if (c > d) 
-                std::swap(c, d);
-            if (a > c) 
-                std::swap(a, c);
-            if (b > d) 
-                std::swap(b, d);
-            if (b > e) 
-                std::swap(b, e);
-            if (a > b) 
-                std::swap(a, b);
-            if (c > e) 
-                std::swap(c, e);
-            if (c > d) 
-                std::swap(c, d);
-            if (d > e) 
-                std::swap(d, e);
-            if (c > d) 
-                std::swap(c, d);
-            return d;
-        }
-        // returns the median of medians of all elements contained between high and low, inclusive.
-};
-#endif
+    int result = quickSelect(medians, medians.begin(), medians.end(), medians.size() / 2);
+    return result;
+}
+
+int medianOfFive(int a, int b, int c, int d, int e)
+{
+    if (a > b) 
+        std::swap(a, b);
+    if (c > d) 
+        std::swap(c, d);
+    if (a > c) 
+        std::swap(a, c);
+    if (b > d) 
+        std::swap(b, d);
+    if (b > e) 
+        std::swap(b, e);
+    if (a > b) 
+        std::swap(a, b);
+    if (c > e) 
+        std::swap(c, e);
+    if (c > d) 
+        std::swap(c, d);
+    if (d > e) 
+        std::swap(d, e);
+    if (c > d) 
+        std::swap(c, d);
+    return d;
+}
