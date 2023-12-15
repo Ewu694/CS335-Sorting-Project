29cf3d9970cf8fbed9b8987f1cc8aae3f0da8a1a
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 02:46:28 -0500

    Update HalfSelectionSort.hpp

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -3,55 +3,37 @@
 #include <chrono>
 
 int halfSelectionSort(std::vector<int>& nums, int& duration) {
-    // start time calculation from here
     auto start = std::chrono::steady_clock::now();
 
     std::size_t size = nums.size();
-
-    // input is too big, don't use this method
-    if (size > 50000) {
-        std::cout << "This input was too large for selection sort. Size: " << size << "\n";
+    if (size > 50000) //If Input is too large, exit
+    {
+        std::cout << "Input was too large for selection sort. Size: " << size << "\n";
         return -1;
     }
-
-
-    for (size_t i = 0; i < size / 2 + 1; i++) {
-        // initialize minIndex (index of minimum element) and index (general index for nested loop)
+    
+    for (size_t i = 0; i < size / 2 + 1; i++) 
+    {
         size_t minIndex = i;
         size_t index = i;
-
-        // find minimum for non-sorted portion of array
-        for (auto it = nums.begin() + i; it != nums.end(); it++) {
+        for (auto it = nums.begin() + i; it != nums.end(); it++) // Finds minimum for non-sorted portion of array
+        {  
             int num = *it;
             int min = nums[minIndex];
-
-            if (num < min) {
+            if (num < min) 
                 minIndex = index;
-            }
-
             index++;
         }
 
-        // swap ith element with minimum
-        std::swap(nums[i], nums[minIndex]);
+        std::swap(nums[i], nums[minIndex]); //swap ith element with minimum
     }
-
-    // calculate time elapsed
+    
     auto end = std::chrono::steady_clock::now();
     auto diff = end - start;
-
     duration = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
 
-    // print half-sorted vector to console
-//    for (int num : nums) {
-//        std::cout << num << " ";
-//    }
-//    std::cout << std::endl;
-
-    // return median
-    if (size % 2 == 0) {
+    if (size % 2 == 0) 
         return nums[size / 2 - 1];
-    } else {
+    else 
         return nums[nums.size() / 2];
-    }
 }
