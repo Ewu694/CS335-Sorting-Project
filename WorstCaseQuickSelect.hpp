7e02aefa4cf7f391d56dc2727bc47a4a060ded51
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 01:53:23 -0500

    Update WorstCaseQuickSelect.hpp
    Worst Case for Quick Select

--- WorstCaseQuickSelect.hpp
+++ WorstCaseQuickSelect.hpp
@@ -1,31 +1,12 @@
-#ifndef WORSTCASEQUICKSELECT_HPP
-#define WORSTCASEQUICKSELECT_HPP
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <random>
-#include <chrono>
 
-class WorstCaseQuickSelect
+std::vector<int> worstCaseQuickSelect (void)
 {
-    public:
-        std::vector<int>& worstCaseQuickSelect(void)
-        // worstCaseQuickSelect generates a worst-case input for a quickselect that uses median-of-3 partitioning. The input it generates must be of length 20,000, and contain each number from 1-20000 once.
-        {
-            static std::vector<int> worstCaseInput(20000);
-    
-            // Generating the worst-case input
-            for (int i = 0; i < 20000; ++i) {
-                worstCaseInput[i] = i + 1;
-            }
-    
-            // Shuffle the elements to avoid a sorted input (for worst-case)
-            std::random_device rd;
-            std::mt19937 g(rd());
-            std::shuffle(worstCaseInput.begin(), worstCaseInput.end(), g);
-            
-            return worstCaseInput;
-        }
-};
-#endif
+    int min = 1;
+    int max = 20000;
+    std::vector<int> data;
+    for (int i = 20000; i > 0; --i)
+        data.push_back(i);
+    return data;
+}
