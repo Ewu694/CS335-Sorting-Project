eb16b2f369f5e2ace38feba567b707b438467edf
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-12-15 02:16:04 -0500

    Update HalfSelectionSort.hpp

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -2,38 +2,56 @@
 #include <iostream>
 #include <chrono>
 
-int halfSelectionSort(std::vector<int>& nums, int& duration) 
-{
+int halfSelectionSort(std::vector<int>& nums, int& duration) {
+    // start time calculation from here
     auto start = std::chrono::steady_clock::now();
 
     std::size_t size = nums.size();
-    if (size > 50000) // If input is too large:
-    {
-        std::cout << "Input was too large for selection sort. Size: " << size << std::endl; 
+
+    // input is too big, don't use this method
+    if (size > 50000) {
+        std::cout << "This input was too large for selection sort. Size: " << size << "\n";
         return -1;
     }
 
-    for (size_t i = 0; i < size / 2 + 1; i++) 
-    {
+
+    for (size_t i = 0; i < size / 2 + 1; i++) {
+        // initialize minIndex (index of minimum element) and index (general index for nested loop)
         size_t minIndex = i;
         size_t index = i;
-        for (auto iterator = nums.begin() + i; iterator != nums.end(); ++iterator) 
-        {
-            int num = *iteratorerator;
+
+        // find minimum for non-sorted portion of array
+        for (auto it = nums.begin() + i; it != nums.end(); it++) {
+            int num = *it;
             int min = nums[minIndex];
-            if (num < min) 
+
+            if (num < min) {
                 minIndex = index;
+            }
+
             index++;
         }
-        std::swap(nums[i], nums[minIndex]); //swap iterator's element with minimum
+
+        // swap ith element with minimum
+        std::swap(nums[i], nums[minIndex]);
     }
+
+    // calculate time elapsed
     auto end = std::chrono::steady_clock::now();
     auto diff = end - start;
 
     duration = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
 
-    if (size % 2 == 0) 
+    // print half-sorted vector to console
+//    for (int num : nums) {
+//        std::cout << num << " ";
+//    }
+//    std::cout << std::endl;
+
+    // return median
+    if (size % 2 == 0) {
         return nums[size / 2 - 1];
-    else
+    } else {
         return nums[nums.size() / 2];
+    }
 }
